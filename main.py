b947bdbca71f545fc1c1d178ccd35d7950efb25d
Varun Vyas <97342196+LVarunL@users.noreply.github.com>
2024-04-22 22:47:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -111,5 +111,5 @@ async def read_root():
 #if __name__ == "__main__":
 #    uvicorn.run("main:app", host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8001)
+# if __name__ == "__main__":
+#     uvicorn.run(app, host="127.0.0.1", port=8001)
